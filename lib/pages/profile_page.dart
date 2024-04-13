9358eddd6d31a01defceb2cb6ef8a543082895ff
Pratik Bhattarai <149646839+Pratik-Bhattarai77@users.noreply.github.com>
2024-04-14 01:35:53 +0545

    changes in pacakage

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
