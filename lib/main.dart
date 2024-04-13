dc8ba1bbe4c0f5b20fb8db897505128793d5ded7
Pratik Bhattarai <149646839+Pratik-Bhattarai77@users.noreply.github.com>
2024-04-14 00:06:07 +0545

    Khalti integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:application/pages/Auth_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:khalti_flutter/khalti_flutter.dart'; // Import Khalti package
 import 'firebase_options.dart';
 
 void main() async {
@@ -12,13 +13,21 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AuthPage(),
+    return KhaltiScope(
+      publicKey:
+          'test_public_key_b4a66361b76b4349bcfca4e30e8789f6', // Replace with your actual public key
+      enabledDebugging: false, // Set to false in production
+      builder: (context, navKey) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          navigatorKey: navKey,
+          home: const AuthPage(),
+        );
+      },
     );
   }
 }
